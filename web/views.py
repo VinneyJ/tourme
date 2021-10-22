4b17fa7ddd86b906a920f066aa90b9fc9dfea06c
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-10-22 07:08:28 +0300

    mvp view

--- web/views.py
+++ web/views.py
@@ -1,7 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Place
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        place = request.form.get('place')
+
+        if len(place) < 1:
+            flash('place name is too short!', category='error')
+        else:
+            new_place = Place(data=note, user_id=current_user.id)
+            db.session.add(new_place)
+            db.session.commit()
+            flash('new place is added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-place', methods=['POST'])
+def delete_place():
+    place = json.loads(request.data)
+    placeId = place['placeId']
+    place = Place.query.get(placeId)
+    if place:
+        if place.user_id == current_user.id:
+            db.session.delete(place)
+            db.session.commit()
+
+    return jsonify({})
