f0ea5ad8e6b5d8ed3bb5f1ec8d2d2022dcc6bded
VinneyJ <vincentjayden49@gmail.com>
2021-11-04 02:25:36 +0300

    add home functionality comment

--- web_dynamic/views.py
+++ web_dynamic/views.py
@@ -1,3 +1,6 @@
+"""
+This files handles home features like display a list of tour guides.
+"""
 from flask import Blueprint, render_template, url_for, request, flash, redirect
 from .db.models import User, User_info, Post, Session
 #from flask_login import login_required, current_user
