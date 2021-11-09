19a5cfc416975f2bada80046892a633bdd89b780
VinneyJ <vincentjayden49@gmail.com>
2021-11-09 06:16:28 +0300

    file clean up

--- app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from web_dynamic import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000, debug=True)
